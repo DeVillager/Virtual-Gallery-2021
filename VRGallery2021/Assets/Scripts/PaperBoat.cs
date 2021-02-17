ed0ffcd05c8f60aae3a828d5783556ef7e6bf89a
Kirage <tommi.m@hotmail.com>
2021-02-17 17:17:09 +0200

    rope wip, fixed models

--- VRGallery2021/Assets/Scripts/PaperBoat.cs
+++ VRGallery2021/Assets/Scripts/PaperBoat.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using BNG;
+using UnityEditor.UIElements;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PaperBoat : MonoBehaviour
 {
     private float _yrot;
     private float _time;
+    private float _sailDirTime;
     private Grabbable _grabbable;
+    public float rotationDir;
 
     public Rigidbody rb;
     public float sailTime = 4f;
     public int rotations = 4;
+    public float rotationChance = 0.2f;
 
     [SerializeField] private float speed = 1f;
 
@@ -27,8 +32,21 @@ public class PaperBoat : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        transform.Rotate(0f, 180f, 0f);
+    }
+
     private void Sail()
     {
+        _sailDirTime += Time.deltaTime;
+        if (_sailDirTime > sailTime)
+        {
+            sailTime = Random.Range(0f, 2f);
+            rotationDir = Random.Range(0f, 1f) < rotationChance ? 1 : -1;
+            _sailDirTime = 0;
+        }
+        
         if (_time < sailTime / rotations)
         {
             _time += Time.deltaTime;
@@ -36,7 +54,8 @@ public class PaperBoat : MonoBehaviour
         }
         else
         {
-            transform.Rotate(0f, 360f / rotations, 0f);
+            // rotationDir = Random.Range(0f, 1f) < rotationChance ? 1 : -1;
+            transform.Rotate(0f, 360f * rotationDir / rotations, 0f);
             _time = 0f;
         }
     }
